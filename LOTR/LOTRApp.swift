c36aa2c0d322ecaa84806205ec28056664722cbe
vishnuprasad <vishnup@yeswearemad.com>
2025-05-30 14:34:43 +0530

    Initial Commit

---
+++ LOTR/LOTRApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRApp.swift
+//  LOTR
+//
+//  Created by qbuser on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
